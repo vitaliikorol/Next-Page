986b4223d58b240905669afa388dc916e374c619
Vitalii Korol <vitaliikorol@192-168-0-104.rdsnet.ro>
2020-06-15 13:01:38 +0300

    Updates

---
+++ precache-manifest.dfba5be7b235e0dbb096cd55a3b78857.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "de19bdf766453cb90a82c7ba514decb3",
+    "url": "/next-page/index.html"
+  },
+  {
+    "revision": "7e4e71dd6124a31fb8be",
+    "url": "/next-page/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e2a94430e57df5990303",
+    "url": "/next-page/static/js/2.62925e5c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/next-page/static/js/2.62925e5c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e4e71dd6124a31fb8be",
+    "url": "/next-page/static/js/main.a625e2b0.chunk.js"
+  },
+  {
+    "revision": "6bc446b526f137a2eb73",
+    "url": "/next-page/static/js/runtime-main.04ba434a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/next-page/static/media/logo.5d5d9eef.svg"
+  }
+]);
